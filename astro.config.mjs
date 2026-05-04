eb2787b6e3dc8ec77f71e322584e6899083ae904
Hachikoi-Marine <vip.hd777@gmail.com>
2026-05-03 23:39:03 -0600

    removed deprecated env & weird images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: vercel(),
+  output: 'server',
+  adapter: vercel(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
